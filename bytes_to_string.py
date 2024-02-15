34a252a530c6f30d53604e48ec60d6b8278f2fa4
Pedro Arriola <pedroparty310@gmail.com>
2024-02-15 15:54:36 -0600

    Inciso 2 finalizado

--- bytes_to_string.py
+++ bytes_to_string.py
@@ -4,7 +4,7 @@ Carnet: 20188
 Inciso 2
 '''
 
-def bytes_to_ascii(bytes_list):
+def bytes_to_string(bytes_list):
     ascii_list = [int(byte, 2) for byte in bytes_list]
     return ''.join(chr(ascii_char) for ascii_char in ascii_list)
 
@@ -20,9 +20,9 @@ test_bytes_2 = ['01101000', '01101111', '01101100', '01100001']  # 'hola'
 test_bytes_3 = ['01110000', '01100101', '01110010', '01110010', '01101111']  # 'perro'
 
 
-text_1 = bytes_to_ascii(test_bytes_1)
-text_2 = bytes_to_ascii(test_bytes_2)
-text_3 = bytes_to_ascii(test_bytes_3)
+text_1 = bytes_to_string(test_bytes_1)
+text_2 = bytes_to_string(test_bytes_2)
+text_3 = bytes_to_string(test_bytes_3)
 
 print(f"Representación de texto de bytes '{test_bytes_1}': {text_1}")
 print(f"Representación de texto de bytes '{test_bytes_2}': {text_2}")
